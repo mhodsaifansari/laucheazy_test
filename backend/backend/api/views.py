657a0052089d833c33070dac3b59db54853dccc6
mhodsaifansari <mhodsaif.sps@gmail.com>
2023-10-30 14:23:31 +0530

    added: user serializer updated: index view(response has user details)

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from api.serializer import *
 
 # Create your views here.
 @api_view(['GET', 'POST'])
 def index(request):
+    seralizer_data = AccountSeralizer(request.user)
     return Response(
         {
             'message': "User successfully authenticated",
+            'user': seralizer_data.data
         },
         status=status.HTTP_200_OK,
     )
